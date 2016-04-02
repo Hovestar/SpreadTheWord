c0cab6707e5992568b298b08c752bc4ef948f9bc
Alexander Curtiss <apccurtiss@gmail.com>
2016-04-02 23:58:29 +0000

    Added receive.js

--- src/receive.js
+++ src/receive.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var noble = require('noble');
 
 noble.on('stateChange', function(state) {
@@ -9,21 +10,140 @@ noble.on('stateChange', function(state) {
 });
 
 noble.on('discover', function(peripheral) {
-  console.log('peripheral discovered (' + peripheral.id +
-              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
-              ' connectable ' + peripheral.connectable + ',' +
-              ' RSSI ' + peripheral.rssi + ':');
-  console.log('\thello my local name is:');
-  console.log('\t\t' + peripheral.advertisement.localName);
-  console.log('\tcan I interest you in any of the following advertised services:');
-  console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
+  if (peripheral.advertisement.localName === 'spartacus') {
+    noble.stopScanning();
 
-  var serviceData = peripheral.advertisement.serviceData;
-  if (serviceData && serviceData.length) {
-    console.log('\there is my service data:');
-    for (var i in serviceData) {
-      console.log('\t\t' + JSON.stringify(serviceData[i].uuid) + ': ' + JSON.stringify(serviceData[i].data.toString('hex')));
+    console.log('peripheral with ID ' + peripheral.id + ' found');
+    var advertisement = peripheral.advertisement;
+
+    var localName = advertisement.localName;
+    var txPowerLevel = advertisement.txPowerLevel;
+    var manufacturerData = advertisement.manufacturerData;
+    var serviceData = advertisement.serviceData;
+    var serviceUuids = advertisement.serviceUuids;
+
+    if (localName) {
+      console.log('  Local Name        = ' + localName);
     }
+
+    if (txPowerLevel) {
+      console.log('  TX Power Level    = ' + txPowerLevel);
+    }
+
+    if (manufacturerData) {
+      console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+    }
+
+    if (serviceData) {
+      console.log('  Service Data      = ' + serviceData);
+    }
+
+    if (serviceUuids) {
+      console.log('  Service UUIDs     = ' + serviceUuids);
+    }
+
+    console.log();
+
+    explore(peripheral);
   }
-  console.log();
 });
+
+function explore(peripheral) {
+  console.log('services and characteristics:');
+
+  peripheral.on('disconnect', function() {
+    process.exit(0);
+  });
+
+  peripheral.connect(function(error) {
+    peripheral.discoverServices([], function(error, services) {
+      var serviceIndex = 0;
+
+      async.whilst(
+        function () {
+          return (serviceIndex < services.length);
+        },
+        function(callback) {
+          var service = services[serviceIndex];
+          var serviceInfo = service.uuid;
+
+          if (service.name) {
+            serviceInfo += ' (' + service.name + ')';
+          }
+          console.log(serviceInfo);
+
+          service.discoverCharacteristics([], function(error, characteristics) {
+            var characteristicIndex = 0;
+
+            async.whilst(
+              function () {
+                return (characteristicIndex < characteristics.length);
+              },
+              function(callback) {
+                var characteristic = characteristics[characteristicIndex];
+                var characteristicInfo = '  ' + characteristic.uuid;
+
+                if (characteristic.name) {
+                  characteristicInfo += ' (' + characteristic.name + ')';
+                }
+
+                async.series([
+                  function(callback) {
+                    characteristic.discoverDescriptors(function(error, descriptors) {
+                      async.detect(
+                        descriptors,
+                        function(descriptor, callback) {
+                          return callback(descriptor.uuid === '2901');
+                        },
+                        function(userDescriptionDescriptor){
+                          if (userDescriptionDescriptor) {
+                            userDescriptionDescriptor.readValue(function(error, data) {
+                              if (data) {
+                                characteristicInfo += ' (' + data.toString() + ')';
+                              }
+                              callback();
+                            });
+                          } else {
+                            callback();
+                          }
+                        }
+                      );
+                    });
+                  },
+                  function(callback) {
+                        characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
+
+                    if (characteristic.properties.indexOf('read') !== -1) {
+                      characteristic.read(function(error, data) {
+                        if (data) {
+                          var string = data.toString('ascii');
+
+                          characteristicInfo += '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
+                        }
+                        callback();
+                      });
+                    } else {
+                      callback();
+                    }
+                  },
+                  function() {
+                    console.log(characteristicInfo);
+                    characteristicIndex++;
+                    callback();
+                  }
+                ]);
+              },
+              function(error) {
+                serviceIndex++;
+                callback();
+              }
+            );
+          });
+        },
+        function (err) {
+          peripheral.disconnect();
+        }
+      );
+    });
+  });
+}
