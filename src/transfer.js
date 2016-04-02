b8b051bc313f083332b881094425b7af25ff3bb4
Izaak Weiss <izwe5504@colorado.edu>
2016-04-02 11:26:24 -0600

    improved formatting, added names

--- src/transfer.js
+++ src/transfer.js
@@ -4,20 +4,21 @@ var noble = require('noble');
 var async = require('async');
 
 function periDisc(peri){//Peripherial Discovered
-	console.log("ID:"+peri.id);
-	peri.once('servicesDiscover',servDisc);
-	peri.on('disconnect', function(){console.log("EXITING!");});
-	peri.once('connect', function(){console.log("Connected!")});
+	//console.log("ID:"+peri.id);
+	var advert = peri.advertisement;
+	var name = advert.localName;
+	peri.on('disconnect', function(){console.log(peri.id+"\t"+name+"\tExited");});
+	peri.once('connect', function(){console.log(peri.id+"\t"+name+"\tConnected");});
 	peri.connect();
 	peri.discoverServices();
 }
 
 function servDisc(serv){ //Service Discovered
-	console.log("Found One!");
-	console.log(serv);
+	//console.log("Found One!");
+	//console.log(serv);
 	if(serv.length != 0)
 	{
-		serv.discoverIncludedServices();
+		//serv.discoverIncludedServices();
 		serv.discoverCharacteristics();
 		serv.once('characteristicsDiscover',charDisc);
 	}
