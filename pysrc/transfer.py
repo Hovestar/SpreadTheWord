50ef0e7aaba14bd20d9c07dfda1cdc0be95d79c7
Izaak Weiss <izwe5504@colorado.edu>
2016-04-03 03:05:20 -0600

    python

--- pysrc/transfer.py
+++ pysrc/transfer.py
@@ -2,28 +2,30 @@ import socket
 import thread
 import bluetooth
 
-
-
 def hostServer(mac,port):
 	backlog = 1
 	size = 1024
 	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 	sock.bind(("",port))
 	sock.listen(backlog)
-	bluetooth.advertise_service(sock,'spartacus',description = mac);
+	bluetooth.advertise_service(sock,'spartacus',description = mac)
 	s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 	s.bind((mac,port))
 	s.listen(backlog)
-	try:
-		client, address = s.accept()
-		while 1:
+	while True:
+		try:
+			client, address = s.accept()
 			data = client.recv(size)
 			if data:
 				print(data)
 				client.send(data)
-	except:	
-		print("Closing socket")	
+		except Exception:
+			print("Closing socket")	
+			break
+	try:
 		client.close()
+	except Exception, e:
+		pass
 	s.close()
 	sock.close()
 
@@ -36,15 +38,15 @@ def beClient(mac,port,text):
 def sendOut(port,text):
 	backlog = 1
 	size = 1024
-	#while(1): # Bad practice but essentially keeps finding devices and sending its data?
+	#Needs a loop somehow
 	others = bluetooth.find_service(name='spartacus')
 	for other in others:
 		thread.start_new_thread( beClient, (other['host'],port,text) )
 
 if __name__=="__main__":
 	mac = # this intentionally will raise an excception. Put in the bluetooth address
-	port = 3 # if you change change on every freaking machine
+	port = 3 # if you change, change on every freaking machine
 	thread.start_new_thread(hostServer,(mac,port))
 	text = ''
 	if(text):
-		sendOut(port,text);
+		sendOut(port,text)
